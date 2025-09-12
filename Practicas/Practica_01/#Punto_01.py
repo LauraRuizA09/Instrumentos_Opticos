9a10c710190fa20fbbd829d6500243dd71a461ab
LauraRuizA09 <lruizar@unal.edu.co>
2025-09-11 21:14:23 -0500

    Metodo espectro angular
    Algo en A[p,q,z] tiene un error no da le patron aun, puede ser algo del sampleo ya que se esta perdiendo informacion

--- Practicas/Practica_01/#Punto_01.py
+++ Practicas/Practica_01/#Punto_01.py
@@ -1,41 +1,46 @@
 #Punto_01
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 #Metodo Espectro Angular
 
 # ---------- Parámetros ----------
 
 #-----Muestreo Horizontal-------
-Nx = 512        # número de muestras por eje (es mejor utilizar un numero mayor de muestras para mejorar el muestreo)
-Lx = 10.0       # tamaño físico de la ventana (mm)
-dx = Lx / Nx     # paso espacial Δ
-dfx = 1.0 / Lx   # paso en frecuencia Δf
+Nx = 400        # número de muestras por eje (es mejor utilizar un numero mayor de muestras para mejorar el muestreo)
+Lx = 10        # tamaño físico de la ventana (mm)
+dx = Lx / Nx    # paso espacial Δ
+dfx = 1 / Lx    # paso en frecuencia Δf
 
 #-----Muestreo Vertical-------
-Ny = 512        # número de muestras por eje (es mejor utilizar un numero mayor de muestras para mejorar el muestreo)
-Ly = 10.0       # tamaño físico de la ventana (mm)
-dy = Ly / Ny     # paso espacial Δ
-dfy = 1.0 / Ly   # paso en frecuencia Δf
+Ny = 400        # número de muestras por eje (es mejor utilizar un numero mayor de muestras para mejorar el muestreo)
+Ly = 10         # tamaño físico de la ventana (mm)
+dy = Ly / Ny    # paso espacial Δ
+dfy = 1 / Ly    # paso en frecuencia Δf
 
 # ---------- Coordenadas espaciales ----------
 n = np.arange(Nx)  # Contadores
 m = np.arange(Ny)
-x = n * dx                # x = Δx * n
-y = m * dx                # y = Δy * m
+#x = n * dx                # x = Δx * n
+#y = m * dx                # y = Δy * m
+#X, Y = np.meshgrid(x, y)
+
+x = (np.arange(Nx) - Nx/2) * dx
+y = (np.arange(Ny) - Ny/2) * dy
 X, Y = np.meshgrid(x, y)
 
 # ---------- Coordenadas de frecuencia ----------
 p = np.arange(Nx) #Contadores
-q = np.arange(Ny) 
-fx = p * dfx               # fx = Δfx * p
+q = np.arange(Ny)
+fx = p * dfx               # fx = Δfx * p   
 fy = q * dfy               # fy = Δfy * q
 FX, FY = np.meshgrid(fx, fy)
 
 # ---------- Campo inicial U(x,y,0) ----------
 
 # Abertura circular de radio r0 (amplitud 1 dentro)
-r0 = 1.0  # mm
+r0 = 2 # mm
 aperture = (X**2 + Y**2) <= r0**2  #Aqui lo que hacemos es comparar los valores de x y y si estan dentro o fuera del circulo
                                    #si estan dentro su valor es 1 (true) y estan fuera es 0 (false)
 
@@ -51,7 +56,7 @@ U0 = aperture.astype(np.complex128) #Como la funcion anteriro retorna valores bo
 # matematicos y fisico llegamos a esa relacion de aplicar FFT, computacionalmente
 # usamos fft2 debido a que estamos trabajndo en dos dimensiones
 
-A0 = np.fft.fft2(U0) * (dx**2)
+A0 = np.fft.fft2(U0) * (dx*dy)
 
 # ---------- A[p,q,z] ----------
 
@@ -59,7 +64,9 @@ lam_nm = 650.0           # longitud de onda en nanómetros
 lam_mm = lam_nm * 1e-6   # conversión a milímetros
 k = (2*np.pi) / lam_mm
 z = 10                   # en mm
-Az = A0 * (np.exp(1j*z*k*np.sqrt(1-(lam_mm*dfx*dfy)*(FX**2 + FY**2))))
+w = (lam_mm*dfx*dfy)**2
+h = np.sqrt(1-((w)*(FX**2 + FY**2)))
+Az = A0 * (np.exp(1j*z*k*h))
 
 
 # ---------- U[n,m,z] por IFFT ----------
@@ -68,10 +75,20 @@ Az = A0 * (np.exp(1j*z*k*np.sqrt(1-(lam_mm*dfx*dfy)*(FX**2 + FY**2))))
 # del espacio de frecuencias al espacio dimensional por eso utilizamos
 # la transformada inversa de fourier de la funcion del espectro angular
 
-Uz = np.fft.fft2(Az) * (dfx*dfy)
+Uz = np.fft.ifft2(Az) 
 
 # ---------- Re-ordenar el campo U[n,m,z] ----------
 
 # Por la teoria vista de analisis de muestreo vimos que realmente no estamos calculando 
 # el orden correcto de nuetsro campo a la salida es decir este esta en posiciones incorrectas
 # por ende debemos hacer algo para organizarlo, debemos shiftearlo para que ahora si este centrado
+
+# Usamos fftshift para centrar el campo en el dominio espacial
+Uz_shifted = np.fft.fftshift(Uz)
+
+# ---------- Visualizar el resultado ----------
+extent = [-Lx/2, Lx/2, -Ly/2, Ly/2]
+plt.imshow((np.abs(Uz_shifted))**2, extent=extent, origin='lower')
+plt.colorbar()
+plt.title("Intensidad |$U(x,y,z)|^2$ a $z$=$10mm$")
+plt.show()
