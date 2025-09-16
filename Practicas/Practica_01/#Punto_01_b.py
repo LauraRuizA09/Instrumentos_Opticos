b04f1ffbbc73bf930f04f27bc5b514a2f0a0dd85
LauraRuizA09 <lruizar@unal.edu.co>
2025-09-16 13:35:12 -0500

    update

--- Practicas/Practica_01/#Punto_01_b.py
+++ Practicas/Practica_01/#Punto_01_b.py
@@ -9,15 +9,15 @@ import matplotlib.pyplot as plt
 
 #-----Muestreo Horizontal-------
 Nx = 1024        # número de muestras por eje (es mejor utilizar un numero mayor de muestras para mejorar el muestreo)
-Lx = 10        # tamaño físico de la ventana (mm)
-Lx0= 10      # tamaño del plano de apertura
+Lx = 10     # tamaño físico de la ventana (mm)
+Lx0 = 10     # tamaño del plano de apertura
 dx = Lx / Nx    # paso espacial Δ
 dx0 = Lx0/Nx  # paso en frecuencia Δf
 
 #-----Muestreo Vertical-------
 Ny = 1024        # número de muestras por eje (es mejor utilizar un numero mayor de muestras para mejorar el muestreo)
-Ly = 10         # tamaño físico de la ventana (mm)
-Ly0 = 10    # tamaño del plano de apertura
+Ly = 10       # tamaño físico de la ventana (mm)
+Ly0 = 10   # tamaño del plano de apertura
 dy = Ly / Ny    # paso espacial Δ
 dy0 = Ly0/Ny    # paso en frecuencia Δf
 
@@ -57,28 +57,16 @@ U0 = aperture.astype(np.complex128) #Como la funcion anteriro retorna valores bo
 lam_nm = 650.0           # longitud de onda en nanómetros
 lam_mm = lam_nm * 1e-6   # conversión a milímetros
 k = (2*np.pi) / lam_mm
-z = 10                 # en mm
+z = 400            # en mm
 U0_prima = U0 * np.exp(1j * (k/(2*z)) * ((X0*dx0)**2 + (Y0*dy0)**2))
 
 # ---------- U''[n,m,z] por FFT ----------
 
 U0_prima2 = np.fft.fft2(U0_prima) * (dx0**2)
-U0_prima2_ = np.fft.fftshift(U0_prima2)  # Centramos el espectro de frecuencias
-
-# ---------- Visualización en coordenadas físicas ----------
-plt.figure(figsize=(6,6))
-plt.imshow(np.abs(U0_prima), extent=[x0.min(), x0.max(), y0.min(), y0.max()],
-           cmap="gray", origin="lower")
-plt.title("Apertura cuadrada en coordenadas físicas (mm)")
-plt.xlabel("x [mm]")
-plt.ylabel("y [mm]")
-plt.colorbar(label="Amplitud")
-plt.show()
-
 
 # ---------- Escalar U[n,m,z] ----------
 
-U_z = (np.exp(1j * k * z) / (1j * lam_mm * z)) * np.exp(1j * (k/(2*z)) * (X**2 + Y**2)) * U0_prima2_
+U_z = (np.exp(1j * k * z) / (1j * lam_mm * z)) * np.exp(1j * (k/(2*z)) * ((X*dx)**2 + (Y*dy)**2)) * U0_prima2
 
 # ---------- Re-ordenar el campo U_z ----------
 
@@ -90,7 +78,7 @@ I = np.abs(U_z_)**2
 I = I / np.max(I)  # Normalizar
 
 # ---------- Graficar ----------
-extent = [-Lx/2, Lx/2, -Ly/2, Ly/2]
+extent = [x.min(), x.max(), y.min(), y.max()]
 plt.imshow(I, extent=extent, origin='lower')
 plt.colorbar()
 plt.title("Intensidad |$U(x,y,z)|^2$ a $z$=$10mm$")
