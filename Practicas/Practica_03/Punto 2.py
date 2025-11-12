6dcee602a17f21a479f6f84d948f400e6e6feb7e
Nestor <nemendozar@unal.edu.co>
2025-11-11 20:46:39 -0500

    Entrega 3

---
+++ Practicas/Practica_03/Punto 2.py
@@ -0,0 +1,37 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from Microscopio import microscopio, plano_pupila
+from PIL import Image
+#-------Parámetros Microscopio (todas las distancias en mm)-------
+lamda=533e-6  
+F_TL=200 
+M=20
+F_MO=F_TL/M
+NA=0.25
+RPu=F_MO*NA
+#----------------------Definición del objeto----------------------
+#Coordenadas plano objeto
+Le=.39
+Ln=.39
+Ne=1080
+Nn=1080
+de=Le/Ne
+dn=Ln/Nn
+Eje_Horizontal=np.arange(-Ne/2,Ne/2)
+Eje_Vertical=np.arange(-Nn/2,Nn/2)
+e,n=Eje_Horizontal*de,Eje_Vertical*dn
+E,N=np.meshgrid(e,n) # Rejilla de coordenadas para definir un objeto de manera analítica
+
+#Apartado para cargar un objeto a partir de una imagen blanco y negro
+ruta_imagen = r"Practicas\Practica_03\Test de resolución\7998134\2.bmp" # Reemplaza con tu ruta
+img = Image.open(ruta_imagen).convert('L')
+img_array = np.array(img) / 255.0
+Sen = np.flipud(img_array).astype(complex)
+
+#----------------------Definición del plano pupila--------------------
+X,Y=plano_pupila(lamda,Sen,F_MO, Le, Ln)
+P=(X**2+Y**2<=RPu**2)
+#-----------------------------------------------------------------
+Suv, escala=microscopio(M,Sen,P,Le,Ln)
+plt.imshow(abs(Suv)**2/np.max(abs(Suv)**2),extent=escala,cmap="grey")
+plt.show()
---
+++ Practicas/Practica_03/Punto 3.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from Microscopio import microscopio, plano_pupila
+#-------Parámetros Microscopio (todas las distancias en mm)-------
+lamda=533*1e-6 #mm
+M=20
+F_TL=200 #mm
+F_MO=F_TL/M  #mm
+Le=0.390 #mm
+Ln=0.390 #mm
+#----------------------Definición del objeto----------------------
+#Apartado para cargar un objeto complejo a partir de una matriz de datos
+Sen=np.flipud(np.loadtxt(r"Practicas\Practica_03\MuestrasBio\MuestraBio_E05.csv",delimiter=",",dtype=complex))
+#----------------------Definición de la pupila--------------------
+X,Y=plano_pupila(lamda,Sen, F_MO, Le, Ln)
+D=3
+P=(X**2+Y**2)<=(D/2)
+#----------------Campo en el plano de observación-----------------
+Suv, escala=microscopio(M,Sen,P,Le,Ln)
+A=np.array([[1,2,3],[4,5,6]])
+plt.imshow(Suv.real,extent=escala,cmap="grey_r")
+plt.show()
